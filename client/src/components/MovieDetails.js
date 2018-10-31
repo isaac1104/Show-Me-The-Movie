683ce88d09224016eb8d4b0125506403a8912af6
Isaac Kwon <isaac-kwon@hotmail.com>
2018-10-31 14:52:52 -0700

    placeholder image if poster not available

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -17,7 +17,13 @@ const MovieDetails = props => {
     <Col xs={24} sm={12} md={6} lg={4} xl={4} style={{ marginTop: '15px', marginBottom: '15px' }}>
       <Card
         hoverable
-        cover={<img src={`http://image.tmdb.org/t/p/w185/${props.poster}`} alt='movie poster' style={styles.poster} />}
+        cover={
+          <img
+            src={props.poster ? `http://image.tmdb.org/t/p/w185/${props.poster}` : 'https://via.placeholder.com/300?text=Poster+Not+Available' }
+            alt='movie poster'
+            style={styles.poster}
+          />
+        }
         style={styles.card}
         >
           <Meta
