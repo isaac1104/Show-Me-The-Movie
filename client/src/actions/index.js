dd6c2020a8c3f562bf67437c4e630052a5beddd1
Isaac Kwon <isaac-kwon@hotmail.com>
2018-10-31 10:17:09 -0700

    create action creators and reducer for fetching current user

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { REQUEST_AUTH, AUTH_USER, AUTH_ERROR } from './types';
+
+export const signInUser = () => async dispatch => {
+  dispatch({ type: REQUEST_AUTH, payload: true });
+  try {
+    const request = await axios.get('/api/current_user');
+    const { data } = request;
+    dispatch({ type: AUTH_USER, payload: data });
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid Login Credentials!' });
+  }
+};
---
+++ client/src/reducers/current_user_reducer.js
@@ -0,0 +1,37 @@
+import { REQUEST_AUTH, AUTH_USER, AUTH_ERROR } from '../actions/types';
+
+const INITIAL_STATE = {
+  isFetching: false,
+  data: '',
+  error: ''
+};
+
+const currentUserReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case REQUEST_AUTH:
+      return {
+        ...state,
+        isFetching: action.payload,
+        data: '',
+        error: ''
+      };
+    case AUTH_USER:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+        error: ''
+      };
+    case AUTH_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        data: '',
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default currentUserReducer;
