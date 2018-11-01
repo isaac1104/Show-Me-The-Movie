995e2634f934aade7523e99b20df6efc9b574fbd
isaac1104 <isaac-kwon@hotmail.com>
2018-10-31 17:38:10 -0700

    build

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGOURI,
+  mongodb_URI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIEKEY,
   tmdbApiKey: process.env.TMDB_API_KEY
 };
