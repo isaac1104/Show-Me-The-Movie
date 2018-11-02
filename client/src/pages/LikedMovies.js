c7fab313f064d15e0784c25346aa6526c236558d
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-02 08:27:16 -0700

    render liked movies on LikedMovies.js

--- client/src/pages/LikedMovies.js
+++ client/src/pages/LikedMovies.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import MovieCard from '../components/MovieCard';
 import { connect } from 'react-redux';
+import { Icon, Row, Spin } from 'antd';
 import { fetchLikedMovies } from '../actions';
 
 class LikedMovies extends Component {
@@ -7,11 +9,49 @@ class LikedMovies extends Component {
     this.props.fetchLikedMovies();
   };
 
+  renderLikedMovies() {
+    const { isFetching, data } = this.props.liked_movies;
+
+    if (isFetching) {
+      return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh', width: '100%' }}>
+          <Fragment>
+            <Spin
+              size='large'
+              indicator={ <Icon type='loading' /> }
+            />
+          </Fragment>
+        </div>
+      );
+    }
+
+    if (data.length === 0) {
+      return <h1>You Don't Have Any Liked Movies</h1>
+    }
+
+    if (data) {
+      return data.map(movie => {
+        return (
+          <MovieCard
+            key={movie.movieId}
+            id={movie.movieId}
+            title={movie.title}
+            release_date={movie.release_date}
+            poster={movie.poster}
+            rating={movie.rating}
+          />
+        );
+      });
+    }
+  };
+
   render() {
-    console.log(this.props.liked_movies);
     return (
       <div>
         <h1>Liked Movies</h1>
+        <Row type='flex'>
+          {this.renderLikedMovies()}
+        </Row>
       </div>
     );
   }
