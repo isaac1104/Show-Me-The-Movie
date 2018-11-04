fa2d1da77b9d47891d275d6b28b8c7757addde43
isaac1104 <isaac-kwon@hotmail.com>
2018-11-03 20:28:19 -0700

    set breakpoints for slider on Search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -34,7 +34,45 @@ class Search extends Component {
         infinite: true,
         speed: 1000,
         slidesToShow: 8,
-        slidesToScroll: 8
+        slidesToScroll: 8,
+        responsive: [
+          {
+            breakpoint: 1440,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 6
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       };
       return (
         <Fragment>
