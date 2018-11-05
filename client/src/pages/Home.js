bfa8c85dc090566c3430a35d30ea7910f6e6afc1
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-05 15:36:38 -0800

    add user avatar on home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Avatar } from 'antd';
 import { connect } from 'react-redux';
 import { fetchCurrentUser } from '../actions';
 
 class Home extends Component {
   render() {
-    const { data: { username } } = this.props.current_user;
+    const { data: { username, avatar } } = this.props.current_user;
     return (
       <div>
-        <h1>Welcome, {username ? username.split(' ')[0] : 'Guest'}</h1>
+        <h1><Avatar src={avatar} /> Welcome, {username ? username.split(' ')[0] : 'Guest'}</h1>
       </div>
     );
   };
