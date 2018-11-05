7feec86933e5f68d924cf75492deafd960fc2a3d
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-05 09:06:34 -0800

    create route for fetching popular movies

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { fetchNowPlayingMovies } from '../actions';
+import { fetchNowPlayingMovies, fetchPopularMovies } from '../actions';
 import { Icon, Spin, Tag} from 'antd';
 import Slider from 'react-slick';
 import SearchForm from '../components/SearchForm/SearchForm';
@@ -104,18 +104,113 @@ class Search extends Component {
     }
   };
 
+  renderPopularMovies() {
+    const { isFetching, data: { results } } = this.props.now_playing_movies;
+    if (isFetching) {
+      return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh', width: '100%' }}>
+          <Fragment>
+            <Spin
+              size='large'
+              indicator={ <Icon type='loading' /> }
+            />
+          </Fragment>
+        </div>
+      );
+    }
+
+    if (results) {
+      const settings = {
+        arrows: true,
+        draggable: false,
+        autoplay: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 8,
+        slidesToScroll: 8,
+        responsive: [
+          {
+            breakpoint: 1600,
+            settings: {
+              slidesToShow: 8,
+              slidesToScroll: 8
+            }
+          },
+          {
+            breakpoint: 1440,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 6
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+      return (
+        <Fragment>
+          <Tag color='#1890ff' style={{ marginTop: '15px' }}>Popular Movies</Tag>
+          <Slider {...settings}>
+            {results.map(movie => {
+              return (
+                <div key={movie.id}>
+                  <MovieCard
+                    id={movie.id}
+                    title={movie.title}
+                    release_date={movie.release_date}
+                    poster={movie.poster_path}
+                    rating={movie.vote_average}
+                    width={'100%'}
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+        </Fragment>
+      );
+    }
+  };
+
   render() {
+    console.log(this.props.popular_movies);
     return (
       <Fragment>
         <SearchForm />
         {this.renderNowPlayingMovies()}
+        {/* {this.renderPopularMovies()} */}
       </Fragment>
     );
   }
 }
 
-function mapStateToProps({ now_playing_movies }) {
-  return { now_playing_movies };
+function mapStateToProps({ now_playing_movies, popular_movies }) {
+  return { now_playing_movies, popular_movies };
 };
 
-export default connect(mapStateToProps, { fetchNowPlayingMovies })(Search);
+export default connect(mapStateToProps, { fetchNowPlayingMovies, fetchPopularMovies })(Search);
---
+++ routes/popular_movies_route.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const keys = require('../config/keys');
+const requireAuth = require('../middlewares/requireAuth');
+
+module.exports = app => {
+  app.get('/api/popular_movies', requireAuth, async (req, res) => {
+    try {
+      const request = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${keys.tmdbApiKey}&language=en-US&page=1&region=us`);
+      const { data } = request;
+      res.send(data);
+    } catch (e) {
+      res.send(e);
+    }
+  });
+};
