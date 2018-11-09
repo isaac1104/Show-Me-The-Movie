f7a928fb462db96d42c290dcf66143c3730488b4
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-09 15:11:32 -0800

    change ContentLayout style based on path

--- client/src/components/Layout/ContentLayout.js
+++ client/src/components/Layout/ContentLayout.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Layout } from 'antd';
-
 const { Content } = Layout;
-const style = {
-  layout: {
-    padding: '0 24px',
-    alignItems: 'center',
-    backgroundColor: '#f0f2f5',
-    height: '100vh',
-    overflow: 'hidden'
-  },
-  content: {
-    backgroundColor: '#ffffff',
-    padding: 24,
-    margin: 0,
-    width: '100%',
-    overflowY: 'scroll'
-  }
-};
 
-const ContentLayout = props => {
-  return (
-    <Layout style={style.layout}>
-      <Content style={style.content}>
-        {props.children}
-      </Content>
-    </Layout>
-  );
+class ContentLayout extends Component {
+  render() {
+    const { pathname } = window.location;
+    const style = {
+      layout: {
+        padding: '0 24px',
+        alignItems: 'center',
+        backgroundColor: pathname === '/' ? '#36393f' : '#ffffff',
+        height: '100vh',
+        overflow: 'hidden'
+      },
+      content: {
+        backgroundColor: pathname === '/' ? '#36393f' : '#ffffff',
+        padding: 24,
+        margin: 0,
+        width: '100%',
+        overflowY: pathname === '/' ? 'hidden' : 'scroll'
+      }
+    };
+
+    return (
+      <Layout style={style.layout}>
+        <Content style={style.content}>
+          {this.props.children}
+        </Content>
+      </Layout>
+    );
+  }
 }
 
 export default ContentLayout;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import GoogleButton from '../components/GoogleButton';
+import moviesImage from '../images/movies.jpg';
+import { SimpleImg, SimpleImgProvider } from 'react-simple-img';
 
 const styles = {
   container: {
@@ -8,19 +10,22 @@ const styles = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    backgroundImage: `url(${moviesImage})`
   }
 };
 
 const Home = () => {
   return (
-    <div style={styles.container}>
-      <div>
-        <h1>Show Me The Movie</h1>
-        <h3>Whether You Are Looking For Your Favorite Old Movies, Or In A Mood To Watch Something New You Haven't Watched, We Have It All</h3>
-        <GoogleButton />
+    <SimpleImgProvider>
+      <div style={styles.container}>
+        <div>
+          <h1>Show Me The Movie</h1>
+          <h3>Whether You Are Looking For Your Favorite Old Movies, Or In A Mood To Watch Something New You Haven't Watched, We Have It All</h3>
+          <GoogleButton />
+        </div>
       </div>
-    </div>
+    </SimpleImgProvider>
   );
 };
 
