6c7c17f9aa7ed68ec73f8f28b9f977788b0210fc
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-29 13:33:14 -0800

    add facebook client id & secret

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   mongodbURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIEKEY,
   tmdbApiKey: process.env.TMDB_API_KEY
