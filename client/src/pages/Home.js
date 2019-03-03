3fe2d9b87f877441131238c1ad3b5ff7f8cc96e4
isaac1104 <isaac-kwon@hotmail.com>
2019-03-02 22:50:05 -0800

    display trending movies carousel on Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Avatar, Icon } from 'antd';
 import { connect } from 'react-redux';
 import { fetchTrendingMovies } from '../actions';
+import MovieCarousel from '../components/MovieCarousel';
+import Spinner from '../components/Spinner';
 
 class Home extends Component {
   componentDidMount() {
@@ -9,13 +11,23 @@ class Home extends Component {
   }
 
   render() {
-    console.log(this.props.trending_movies);
-    const { data: { username, avatar } } = this.props.current_user;
+    const { current_user: { data: { avatar, username } }, trending_movies } = this.props;
+
+    if (trending_movies.isFetching) {
+      return <Spinner />;
+    }
+
     return (
       <div>
         <h1><Avatar src={avatar} /> Welcome, {username ? username.split(' ')[0] : 'Guest'}</h1>
         <h1><a href='/search'><Icon type='search' /> Search</a> for a movie to get started!</h1>
         <h1>Here are today's top 20 trending movies</h1>
+        <MovieCarousel
+          type='search'
+          tagColor='#ff4f00'
+          title='Trending'
+          data={trending_movies.data}
+        />
       </div>
     );
   };
