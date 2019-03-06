1b94c1228022e161ad22410875e53563ccfc048e
Isaac Kwon <isaac-kwon@hotmail.com>
2019-03-06 12:45:50 -0800

    add key props to SignInButton

--- client/src/components/SignInButton.js
+++ client/src/components/SignInButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SignInButton = ({ platforms }) => platforms.map(platform => (
-  <a href={`/auth/${platform.toLowerCase()}`}>
+  <a href={`/auth/${platform.toLowerCase()}`} key={platform}>
     <button className={`loginBtn loginBtn--${platform.toLowerCase()}`}>
       Login with {platform}
     </button>
