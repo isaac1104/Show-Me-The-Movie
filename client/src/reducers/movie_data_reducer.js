a626fcf209f200ba4e3f9965dba31c85474f13f8
isaac1104 <isaac-kwon@hotmail.com>
2019-05-21 18:30:27 +0900

    display data

--- client/src/reducers/movie_data_reducer.js
+++ client/src/reducers/movie_data_reducer.js
@@ -19,7 +19,7 @@ const movieDataReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         data: action.payload,
-        isFetching: true,
+        isFetching: false,
         error: ''
       };
     case REJECT_MOVIE_DATA:
