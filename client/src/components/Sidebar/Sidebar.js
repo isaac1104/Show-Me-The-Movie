375f136fb273c852cfc0d90396d7bfa27294a534
isaac1104 <isaac-kwon@hotmail.com>
2021-01-10 15:32:40 -0600

    sidebar width change

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -18,38 +18,49 @@ class Sidebar extends Component {
     } else {
       return ['0'];
     }
-  };
+  }
 
   renderSidebar() {
     const { data } = this.props.current_user;
     const menuItems = [
-    { path: 'home', icon: 'home', text: 'Home' },
-    { path: 'liked_movies', icon: 'heart', text: 'Liked' },
-    { path: 'search', icon: 'search', text: 'Search' },
-    { path: 'api/signout', icon: 'logout', text: 'Logout' }
+      { path: 'home', icon: 'home', text: 'Home' },
+      { path: 'liked_movies', icon: 'heart', text: 'Liked' },
+      { path: 'search', icon: 'search', text: 'Search' },
+      { path: 'api/signout', icon: 'logout', text: 'Logout' },
     ];
 
     if (data) {
       return (
-        <Sider breakpoint='xl' collapsedWidth='0' className={classes.Sidebar}>
-          <Menu mode='inline' theme='dark' defaultSelectedKeys={this.highlightMenu()} className={classes.SidebarMenu}>
-            {menuItems.map(({ path, icon, text }, i) => {
+        <Sider
+          breakpoint='xl'
+          collapsedWidth={0}
+          className={classes.Sidebar}
+          width={60}
+        >
+          <Menu
+            mode='inline'
+            theme='dark'
+            defaultSelectedKeys={this.highlightMenu()}
+            className={classes.SidebarMenu}
+          >
+            {menuItems.map(({ path, icon }, i) => {
               if (path === 'api/signout') {
                 return (
-                  <Menu.Item key={i} className={classes.SidebarLogoutButton}>
+                  <Menu.Item
+                    key={i}
+                    className={`${classes.SidebarLogoutButton} ${classes.MenuItem}`}
+                  >
                     <a href={`/${path}`}>
                       <Icon type={icon} />
-                      <span className='nav-text'>{text}</span>
                     </a>
                   </Menu.Item>
                 );
               }
 
               return (
-                <Menu.Item key={i}>
+                <Menu.Item className={classes.MenuItem} key={i}>
                   <Link to={`/${path}`}>
                     <Icon type={icon} />
-                    <span className='nav-text'>{text}</span>
                   </Link>
                 </Menu.Item>
               );
@@ -65,10 +76,10 @@ class Sidebar extends Component {
   render() {
     return <Fragment>{this.renderSidebar()}</Fragment>;
   }
-};
+}
 
 function mapStateToProps({ current_user }) {
   return { current_user };
-};
+}
 
 export default connect(mapStateToProps, null)(Sidebar);
